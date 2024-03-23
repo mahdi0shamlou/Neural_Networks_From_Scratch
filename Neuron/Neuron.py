6bfefeb004ac182e9b96cbc5b0d4c250a7d5bd43
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-03-23 10:19:25 -0400

    creat method think and activate function and workingi on train

--- Neuron/Neuron.py
+++ Neuron/Neuron.py
@@ -1,9 +1,27 @@
-import numpy
-import pandas
+import numpy as np
+
 
 class Neuron():
     # first neuron has only one input and it is use for finding a liner regresion
-    def __init__(self, x1):
-        self.Inputs = []
-        self.Inputs.append(x1)
+    def __init__(self):
+        self.w1 = np.random.random()
+        print(self.w1)
+    def Activate_function(self, x):
+        if x > 0:
+            return 1
+        else:
+            return 0
+
+    def train(self, repet, input_data, output_data):
         pass
+
+
+    def think(self, x):
+        x = self.w1 * x
+        x = self.Activate_function(x)
+        return x
+
+
+#-------------------------------- test
+n = Neuron()
+print(n.think(-1))
