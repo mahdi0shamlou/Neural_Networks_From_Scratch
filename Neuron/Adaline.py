75cdf0133b52bf24436650320f17c98766c7f8b9
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-05-12 15:24:45 -0400

    create optimaize methode in adaline neruon

--- Neuron/Adaline.py
+++ Neuron/Adaline.py
@@ -2,7 +2,7 @@ import random
 from matplotlib import pyplot as plt
 import numpy as np
 
-x_data_set = [
+x_data_set = np.matrix([
     [1],
     [2],
     [3],
@@ -17,8 +17,8 @@ x_data_set = [
     [12],
     [13],
     [14]
-]
-y_data_set = [
+])
+y_data_set = np.matrix([
     [2],
     [4],
     [6.1],
@@ -33,4 +33,21 @@ y_data_set = [
     [24],
     [26],
     [28]
-]
+])
+
+def Optimaize(x: np.matrix, y: np.matrix):
+    """
+    this methode get x and y and find best wight for x * w = y
+    we calculate wight with matrix
+    :param x:
+    :param y:
+    :return:
+    """
+    W = x.T.dot(y) / (x.T.dot(x))
+    return W
+
+if __name__ == '__main__':
+    print(x_data_set.shape)
+    print(x_data_set.T.shape)
+
+    pass
