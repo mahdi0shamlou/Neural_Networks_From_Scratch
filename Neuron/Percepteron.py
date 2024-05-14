735e7f4953746b03f4ec4b79dcf8a6593930ac83
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-05-14 15:10:58 -0400

    add some comments

--- Neuron/Percepteron.py
+++ Neuron/Percepteron.py
@@ -7,10 +7,10 @@ def create_data_sets():
     :return:
     """
 
-    x_data_set = [[-2], [2], [3], [4], [5], [6], [7], [10], [-1]]
+    x_data_set = [[-2], [2], [3], [4], [5], [6], [7], [10], [-1]]  # you can replace this with your x_data_set
     y_data_set = [
         [(i[0] * 3) + 1] for i in x_data_set
-    ]
+    ]  # you can replace this with your y_data_sets
     return x_data_set, y_data_set
 
 
@@ -36,8 +36,11 @@ def activate_function(net):
     """
     return 1 / (1 + np.exp(-net))
 
+
+
 if __name__ == '__main__':
-    X_data_set, Y_data_set = create_data_sets()
+
+    X_data_set, Y_data_set = create_data_sets()  # you can disable this line and replace your x and y 
     X_data_set, Y_data_set = create_matrix_from_data_sets(X_data_set, Y_data_set)
     print(f"x data sets : \n {X_data_set}")
     print(f"y data sets : \n {Y_data_set}")
