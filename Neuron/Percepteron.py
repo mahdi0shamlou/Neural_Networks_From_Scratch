cc41316ec71979ac6d5dbf046fdbca4cfa75a3d6
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-05-16 17:37:19 -0400

    complete methode learning and optimaizetion of wight

--- Neuron/Percepteron.py
+++ Neuron/Percepteron.py
@@ -1,5 +1,7 @@
 import numpy as np  # We need Numpy for matrix
 import random
+
+from numpy.distutils.system_info import x11_info
 from sklearn.preprocessing import normalize
 
 def create_data_sets():
@@ -8,7 +10,7 @@ def create_data_sets():
     :return:
     """
 
-    x_data_set = [[-2], [2], [3], [4], [5], [6], [7], [10], [-1], [-4]]  # you can replace this with your x_data_set
+    x_data_set = [[-12], [12], [8], [9], [10], [17], [11], [15], [-10], [-14]]  # you can replace this with your x_data_set
     y_data_set = []
     for i in x_data_set:
         if i[0] > 3:
@@ -47,10 +49,10 @@ def activate_function(net):
 def activate_function_derivative(net):
     """
     computing derivative to the Sigmoid function
-    :param x:
+    :param net:
     :return:
     """
-    return np.exp(-net) / ( (1+np.exp(-net)) * (1+np.exp(-net)) )
+    return np.exp(-net) / ((1 + np.exp(-net)) * (1 + np.exp(-net)))
 
 
 def create_w(n):
@@ -61,23 +63,76 @@ def create_w(n):
     """
     w = []
     for i in range(0, n+1):
-        w.append(random.uniform(-1, 1))
+        w.append(random.uniform(-1,1))
     w = np.matrix(w)
     return w
 
 
 def learning(x_data: np.matrix, y_data: np.matrix, n: int, z: int, iteration: int):
-
     w = create_w(n)
     print(f"this is starter Wight : {w}")
-    print(f"this is claculet with {w} -> {w.dot(x_data[0].T)}")
+    '''
+    
+    
+    print(f"this is X-data : {x_data[0]}")
+    print(f"this is Y-data : {y_data[0]}")
+    net = w.dot(x_data[0].T)
+    print(f"this is net of simpel one : {net}")
+    resalut = activate_function(net)
+    print(f"this is resualt of activation methode : {resalut}")
+    error = (y_data.item(0) - resalut)
+    print(f"this is error : {error}")
 
+    net_new = w.item(0) * x_data.item(0)
+    print(f"this is new net {net_new}")
+    new_dif = activate_function_derivative(net_new)
+    print(f"this is diff of activaet {new_dif}")
+
+    print("----------------------------")
+    print(f"this is way we should go {new_dif*error*x_data.item(0)*-1}")
+    
+    ##################################
+    print("\n-----------------------")
+    net = w.dot(x_data.T)
+    print(f"this is all of net : \n{net}\n-----------------------")
+    resalut = activate_function(net)
+    print(f"this is all of resalut : \n{resalut}\n-----------------------")
+    error = y_data.T-resalut
+    print(f"this is error {error}")
+    print(x_data.shape)
+    print(error.shape)
+    print(error.dot(x_data))
+    print(net.T.shape)
+    difs = map(activate_function_derivative, net.T)
+    print(list(difs))
+    difs = activate_function_derivative(net.T)
+    print(difs)
+    print(difs.shape)
+    '''
     for i in range(0, iteration):
-        net = w.dot(x_data.T)
-        print(net)
-        error = y_data - activate_function(net)
-        adjustments = np.dot(x_data.T, error * activate_function_derivative(y_data))
-        print(adjustments)
+        rand_num = random.randint(0, 9)
+        #rand_num = 0
+        net = w.dot(x_data[rand_num].T)
+        resault = activate_function(net)
+        error = y_data[rand_num] - resault
+        w_new_adj = []
+        for z in range(0, n+1):
+            new_nets = w.item(z) * x_data[rand_num].item(z)
+            difs = activate_function_derivative(new_nets)
+            #print(new_nets)
+            #print(difs)
+            adjs_add = error*difs*x_data[rand_num].item(0)
+            w_new_adj.append(adjs_add.item(0))
+            #print(error*difs*x_data[rand_num].item(0))
+        w_new_adj = np.matrix(w_new_adj)
+        w = w + w_new_adj
+        print(w)
+        print(w_new_adj)
+        print(w)
+        print('---------------')
+
+
+        pass
 
     return w
 
@@ -86,8 +141,22 @@ if __name__ == '__main__':
 
     X_data_set, Y_data_set = create_data_sets()  # you can disable this line and replace your x and y
     X_data_set, Y_data_set = create_matrix_from_data_sets(X_data_set, Y_data_set)
-    print(f"x data sets : {X_data_set.shape} \n {X_data_set}")
-    print(f"y data sets : {Y_data_set.shape} \n {Y_data_set}")
-    w = learning(x_data=X_data_set, y_data=Y_data_set, n=1, z=1, iteration=10000)
+    #print(f"x data sets : {X_data_set.shape} \n {X_data_set}")
+    #print(f"y data sets : {Y_data_set.shape} \n {Y_data_set}")
+    w = learning(x_data=X_data_set, y_data=Y_data_set, n=1, z=1, iteration=100000)
+
+    net = w.dot(X_data_set.T)
+    print(net)
+    resault = activate_function(net)
+    print(resault)
+    print(resault.shape)
+    print(Y_data_set.shape)
+    error = Y_data_set.T - resault
+
+    for i in range(0, 9):
+        a = error.item(i)
+        print("%.16f" % a)
+
+    #print(error)
 
 
